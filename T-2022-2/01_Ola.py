77b2be2c8a081889243606fadce067aaba7980d9
PJandl <pjandl@gmail.com>
2022-10-19 17:22:19 -0300

    Delete 01_Ola.py
    Example files moved to new directory.

--- T-2022-2/01_Ola.py
+++
@@ -1,9 +0,0 @@
-#
-# Oficina de Computação Física
-# Prof. Peter Jandl Jr
-#
-# 01_Ola.py
-# Entrada e saida de string no console (da placa).
-#
-nome = input('Qual é o seu nome? ')
-print ('Olá', nome)
