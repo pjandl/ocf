1f58248416782d4f8d563bd46fdb8824bb393453
PJandl <pjandl@gmail.com>
2022-10-23 12:23:57 -0300

    Add files via upload
    Experiment 06 code.

---
+++ T-2022-2/exemplos/06_botao_led_rele.py
@@ -0,0 +1,29 @@
+#
+# Oficina de Computação Física
+# Prof. Peter Jandl Jr
+#
+# 06_botao_led_rele.py
+# Um botão, com manutenção de estado, para controle
+# de um relê, com led externo de sinalização.
+#
+from machine import Pin
+from time import sleep
+
+led = Pin(15, Pin.OUT)
+botao = Pin(12, Pin.IN)
+estado = False
+led.value(estado)
+print('Controle:', estado)
+ultimo = estado
+try:
+    while True:
+        valor = botao.value()
+        if valor and not ultimo:
+            estado = not estado
+            led.value(estado)
+            print('Controle:', estado)
+        ultimo = valor
+        sleep(0.2)
+except KeyboardInterrupt:
+    led.value(0)
+    print('Programa finalizado')
