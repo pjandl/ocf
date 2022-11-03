df160607bff97af02a81f599ba4a88e658974ab3
PJandl <pjandl@gmail.com>
2022-11-03 10:25:24 -0300

    Add files via upload
    Code correction for better temperatura convertion.

--- T-2022-2/exemplos/10_ADC_ntc.py
+++ T-2022-2/exemplos/10_ADC_ntc.py
@@ -3,31 +3,34 @@
 # Prof. Peter Jandl Jr
 #
 # 10_ADC_ntc.py
-# Leitura analógica de um termistor (NTC), com led de alerta para temperatura alta.
+# Leitura analógica de um termistor (NTC), com led de alerta
+# para temperatura alta.
 #
 from machine import Pin, ADC
 from math import log
 from time import sleep
 
+# termistor conectado ao pino 1 (ADC0)
 ntc = ADC(0)
-BETA = 3950 # correspondente ao termistor usado
+# saida conectada ao led externo
 led = Pin(15, Pin.OUT)
 
-def temperatura(valor_bruto):
-    tempK = 1 / (log(1 / (1023. / valor_bruto - 1)) / BETA + 1.0 / 298.15)
-    tempC = tempK - 273.15
-    return tempK, tempC
+def temperatura(resolucao=1023):
+    tempK = log(10000.0 * (resolucao / ntc.read() - 1))
+    tempK = 1 / (0.001129148 + (0.000234125 + (0.0000000876741 * tempK * tempK)) * tempK)
+    return tempK
+    
+def K_para_C(K):
+  return K - 273.15
 
 try:
     while True:
-        leitura = ntc.read()
-        temp = temperatura(leitura)
-        print("Temperatura:",
-              round(temp[0], 2), "K |",
-              round(temp[1], 2), "˚C")
-        if temp[1] > 27:
+        temp = temperatura()
+        print("Temperatura:", round(temp, 2), "K |",
+              round(K_para_C(temp), 2), "˚C")
+        if temp > 300:
             led.on()
-            print("Alerta de temperatura")
+            print("Alerta de temperatura!!")
         else:
             led.off()
         sleep(4.0)
