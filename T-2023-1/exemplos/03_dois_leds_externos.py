ce2795c91b7d45a5c82c40b9db8ec9fb49b49c46
PJandl <pjandl@gmail.com>
2023-03-13 16:37:44 -0300

    Add files via upload
    Code review for experiment 03.

--- T-2023-1/exemplos/03_dois_leds_externos.py
+++ T-2023-1/exemplos/03_dois_leds_externos.py
@@ -2,16 +2,21 @@
 # Oficina de Computação Física
 # Prof. Peter Jandl Jr
 #
-# 03_led_externo.py
-# Acionamento do led externo à placa.
+# 03_dois_leds_externos.py
+# Acionamento dos leds externos à placa.
 #
 from machine import Pin
 from time import sleep
 
 led1 = Pin(15, Pin.OUT)
+led2 = Pin(13, Pin.OUT)
 
 while True:
     led1.value(1)
     sleep(0.5)
     led1.value(0)
+    sleep(0.5)
+    led2.value(1)
+    sleep(0.5)
+    led2.value(0)
     sleep(0.5)
